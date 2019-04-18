21787d93b46e9c0c8a488415d2efd98527bed89c
Mauro Delazeri <maurodelazeri@gmail.com>
2019-04-18 13:44:30 -0400

    update

--- conanfile.py
+++ conanfile.py
@@ -50,7 +50,14 @@ class MysqlConnectorCConan(ConanFile):
 
         if self.options.with_zlib:
             cmake.definitions["WITH_ZLIB"] = "system"
-       
+
+        #flags =""
+        #if str(self.settings.compiler.libcxx) == "libstdc++":
+        #    flags = "-D_GLIBCXX_USE_CXX11_ABI=0"
+        #elif str(self.settings.compiler.libcxx) == "libstdc++11":
+        #    flags = "-D_GLIBCXX_USE_CXX11_ABI=1"
+        cmake.definitions["CMAKE_CXX_FLAGS"] = "-D_GLIBCXX_USE_CXX11_ABI=0"
+
         cmake.configure(source_dir=self._source_subfolder)
         cmake.build()
         cmake.install()
